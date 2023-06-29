6efda50334f9390a11967fc3a036ea0d69547118
leal <leal@pinquest.cn>
2023-06-29 11:17:31 +0800

    feat: dev server

--- .profile/utils/define-config.js
+++ .profile/utils/define-config.js
@@ -9,9 +9,20 @@ const defaultConfig = {
     extensions: ['.ts', '.tsx', '.jsx', '.json', '.js', '.sass', '.scss'],
   },
   devServer: {
+    noInfo: true,
+    historyApiFallback: true,
+    compress: true,
     port: 3000,
     hot: true,
     host: '0.0.0.0',
+    // https: true,
+    client: {
+      logging: 'none',
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+    },
   },
   env: {},
   lessOptions: {},
